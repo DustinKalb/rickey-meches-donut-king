55e2d41c185f8e514535257dfb302a21cd58e07b
DustinKalb <dustinkalb9078@gmail.com>
2025-11-15 17:28:58 -0600

    added logos needs editing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 import "./globals.css";
-import NavBar from "../components/NavBar/NavBar.jsx";
 
 export default function RootLayout({ children }) {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import styles from "./page.module.css";
 import NavBar from "../components/NavBar/NavBar.jsx";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <div className={styles.slideShow}>
+        <Image className={styles.logo} src="/logo.png" height={150} width={150} quality={100} />
         <NavBar />
         <div className={styles.titleBox}>
-          <h1 className={styles.storeName}>Rickey Meche's Donut King</h1>
+          {/*<h1 className={styles.storeName}>Rickey Meche's Donut King</h1>*/}
+          <Image src="/logowords.png" height={243.77} width={500} quality={100} />
           <div className={styles.catchPhrase}>Making Your Day a Little Sweeter</div>
         </div>
       </div>
