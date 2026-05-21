58a0489551ace9f295bf22fd41b6fabfeb5bd672
DustinKalb <dustinkalb9078@gmail.com>
2026-05-21 00:26:32 -0500

    messed with styling

--- src/components/HoursOfOperation/HoursOfOperation.jsx
+++ src/components/HoursOfOperation/HoursOfOperation.jsx
@@ -1,20 +1,26 @@
+import "./HoursOfOperation.css";
+
 function HoursOfOperation() {
 	return (
 		<div>
-		  <div>Sunday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Monday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Tuesday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Wednesday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Thursday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Friday</div>
-		  <div>4:00am - 1:30pm</div>
-		  <div>Saturday</div>
-		  <div>4:00am - 1:30pm</div>
+		  <div>Closed Opens tomorrow at 4:00am</div>
+		  <div>Business Hours</div>
+	      <div className="hoursOfOperation">
+		    <div>Sunday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Monday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Tuesday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Wednesday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Thursday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Friday</div>
+		    <div>4:00am - 1:30pm</div>
+		    <div>Saturday</div>
+		    <div>4:00am - 1:30pm</div>
+		  </div>
 		</div>
 	)
 }
