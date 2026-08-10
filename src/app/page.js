6e70ac9701fac746544a42ba534fba48af94a0c1
DustinKalb <dustinkalb9078@gmail.com>
2026-08-10 00:53:41 -0500

    started with global margins

--- src/app/page.js
+++ src/app/page.js
@@ -16,34 +16,68 @@ export default function Home() {
         <NavBar />
         <div className={styles.titleBox}>
           {/*<h1 className={styles.storeName}>Rickey Meche's Donut King</h1>*/}
-          <Image className={styles.storeName} src="/logofull.png" height={243.77} width={550} quality={100} />
+          <Image
+            className={styles.storeName}
+            src="/logofull.png"
+            height={243.77}
+            width={550}
+            quality={100}
+          />
           <br />
-          <div className={`${styles.catchPhrase} ${pacifico.className}`}>Making Your Day a Little Sweeter</div>
+          <div className={`${styles.catchPhrase} ${pacifico.className}`}>
+            Making Your Day a Little Sweeter
+          </div>
         </div>
       </div>
       <div className={styles.content}>
         <div className={styles.aboutBackground}>
-          <div className={styles.aboutCard}>
-            
-            <div className={styles.aboutText}>
-              <div>About Us</div>
-              <div>My name is Dustin and I work at Meche's Donut King. I get there super early and make a ton of product then I flip donuts for like 12 whole hours.</div>
-            </div>
-            <div className={styles.imageWrapper}>
-              <Image className={styles.slideShow} src="/rickster.jpg" fill quality={100} />
+          <div className={styles.margin}>
+            <div className={styles.aboutCard}>
+              <div className={styles.aboutText}>
+                <div>About Us</div>
+                <div>
+                  My name is Dustin and I work at Meche's Donut King. I get
+                  there super early and make a ton of product then I flip donuts
+                  for like 12 whole hours.
+                </div>
+              </div>
+              <div className={styles.imageWrapper}>
+                <Image
+                  className={styles.slideShow}
+                  src="/rickster.jpg"
+                  fill
+                  quality={100}
+                />
+              </div>
             </div>
           </div>
         </div>
         <div className={styles.hoursBackground}>
           <div className={styles.socialsCard}>
-            <div className={pacifico.className}>Check out our social medias!</div>
-            <div>O O</div>
-          </div>
-          <div className={styles.hoursCard}>
-            <div className={styles.hoursImageWrapper}>
-              <Image className={styles.donut} src="/chocnglaze2.png" width={700} height={700} quality={100} />
+            <div className={pacifico.className}>
+              Check out our social medias!
             </div>
-            <HoursOfOperation />
+            <div>O O</div>
+            <br />
+            <br />
+            <div className={pacifico.className}>
+              <div>"Do you have any hot donuts?"</div>
+              <div>- Most Customers</div>
+            </div>
+          </div>
+          <div className={styles.margin}>
+            <div className={styles.hoursCard}>
+              <div className={styles.hoursImageWrapper}>
+                <Image
+                  className={styles.donut}
+                  src="/chocnglaze2.png"
+                  width={700}
+                  height={700}
+                  quality={100}
+                />
+              </div>
+              <HoursOfOperation />
+            </div>
           </div>
         </div>
         <iframe
