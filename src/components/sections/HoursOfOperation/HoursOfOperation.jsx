541c43821bc82f1e28e3b7387d28a6facf2a40c9
DustinKalb <dustinkalb9078@gmail.com>
2026-08-19 17:37:47 -0500

    finished majority of hours section minus styling

--- src/components/sections/HoursOfOperation/HoursOfOperation.jsx
+++ src/components/sections/HoursOfOperation/HoursOfOperation.jsx
@@ -19,26 +19,31 @@ function HoursOfOperation() {
           <h2 className="h2">Made Daily, Just For You</h2>
           <div className="divider" />
           <div className="column">
-            <div className="hoursText">Lorem Ipsum</div>
-
-            <div className="storeHours">
-              <div>Closed Opens tomorrow at 4:00am</div>
-              <div>Business Hours</div>
-              <div className="hoursOfOperation">
-                <div>Sunday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Monday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Tuesday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Wednesday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Thursday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Friday</div>
-                <div>4:00am - 1:00pm</div>
-                <div>Saturday</div>
-                <div>4:00am - 1:00pm</div>
+            <div className="hoursText">
+              Lorem Ipsum blah blah blah blah blah blah blah blah blah blah blah
+            </div>
+            <div className="storeHoursBorder">
+              <div className="storeHours">
+                <div>Closed Opens tomorrow at 4:00am</div>
+                <div>Business Hours</div>
+                <div className="hoursOfOperation">
+                  <div>Sunday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Monday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Tuesday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Wednesday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Thursday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Friday</div>
+                  <div>4:00am - 1:00pm</div>
+                  <div>Saturday</div>
+                  <div>4:00am - 1:00pm</div>
+                </div>
+                <br />
+                <div>*Times are subject to change.</div>
               </div>
             </div>
           </div>
