e13ca7da79de4f045c848dcb7b5a1e43175f6bc7
DustinKalb <dustinkalb9078@gmail.com>
2026-09-10 17:48:19 -0500

    worked on contact section scaffolding

--- src/components/sections/ContactSection/ContactSection.jsx
+++ src/components/sections/ContactSection/ContactSection.jsx
@@ -3,13 +3,15 @@ import "./ContactSection.css";
 function ContactSection() {
   return (
     <div className="contactBackground">
-      <h3>Contact Us</h3>
+      <h1 className="contactUs">Contact Us</h1>
       <h1>Need To Place An Order?</h1>
-      <div>Questions about orders, hours, or anything else? Just call!</div>
+      <div>Questions about orders, hours, or anything else?</div>
       <div className="contactCard">
-        <div>Give Us A Call</div>
+        <div style={{ fontSize: "50px" }}>O</div>
+        <h1 className="giveUsACall">Give Us A Call</h1>
+        <div>All orders are handled over the phone.</div>
         <div>-</div>
-        <div>(123) 456-7890</div>
+        <h1>(123) 456-7890</h1>
         <div className="callUs">Call Us</div>
       </div>
     </div>
