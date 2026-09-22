9279ad7aaa00c5748d3bdf4c2998bab3d8dfa852
DustinKalb <dustinkalb9078@gmail.com>
2026-09-21 21:10:58 -0500

    worked on structuring for contact section

--- src/components/sections/ContactSection/ContactSection.jsx
+++ src/components/sections/ContactSection/ContactSection.jsx
@@ -1,20 +1,25 @@
 import "./ContactSection.css";
+import { CiPhone } from "react-icons/ci";
 
 function ContactSection() {
   return (
-    <div className="contactBackground">
-      <h1 className="contactUs">Contact Us</h1>
-      <h1>Need To Place An Order?</h1>
-      <div>Questions about orders, hours, or anything else?</div>
-      <div className="contactCard">
-        <div style={{ fontSize: "50px" }}>O</div>
-        <h1 className="giveUsACall">Give Us A Call</h1>
-        <div>All orders are handled over the phone.</div>
-        <div>-</div>
-        <h1>(123) 456-7890</h1>
-        <div className="callUs">Call Us</div>
+    <>
+      <div className="contactBackground">
+        <div className="contactTitle">
+          <h1 className="contactUs">Contact Us</h1>
+          <h1>Need To Place An Order?</h1>
+          <div>Questions about orders, hours, or anything else?</div>
+        </div>
+        <div className="contactCard">
+          <CiPhone className="socialIcon" />
+          <h1 className="giveUsACall">Give Us A Call</h1>
+          <div>All orders are handled over the phone.</div>
+          <div className="divider2" />
+          <h1 className="number">(123) 456-7890</h1>
+          <div className="callUs">Call Us</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
